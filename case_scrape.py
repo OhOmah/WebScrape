b8be7d35bc413d283c689a04362a01d48d5bd7f2
Omar Zaffar <omar.zaffarr@gmail.com>
2025-02-17 22:19:39 -0800

    First push of current data

---
+++ case_scrape.py
@@ -0,0 +1,105 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+import re
+import time
+from datetime import datetime
+import pandas as pd 
+
+'''
+PURPOSE OF THIS FILE:
+This file will take a case HTML table and convert into a pandas dataframe 
+'''
+def case_split(case):
+    case_details_list = []
+    case_parts = case.split("\n")
+    case_number_name = case_parts[0].split(" ", 1)
+     # Extract case type
+    case_type = case_parts[1]
+    # Extract filed date and additional info
+    date_filed_info = case_parts[2].split(" ", 2)
+    case_details_list.append({
+        "CaseNumber": case_number_name[0],
+        "CaseName": case_number_name[1],
+        "CaseType": case_type,
+        "DateFiled": date_filed_info[1],
+        "AdditionalInfo": date_filed_info[2] if len(date_filed_info) > 2 else ''
+    })
+    df = pd.DataFrame(case_details_list)
+
+    return df
+
+def case_scrape(driver, cases, links):
+    '''
+    PURPOSE OF THIS FUNCTION: 
+    This will take a 
+    '''
+    for i in range(len(links)):
+        # 3. Click and get information of the first element in list, store in dataframe then go back
+        link = links[0]
+        link.click()
+        parties = driver.find_element(By.XPATH, '//*[contains(text(), "PARTIES")]')
+        parties.click()
+        time.sleep(5)
+
+        # Scrape the type of case. 
+        table = driver.find_elements(By.XPATH, "//*[contains(@id, 'forms_table')]")
+        columns = table[0].find_elements(By.TAG_NAME, 'td')
+        case_info_df = case_split(columns[0].text)
+
+        # Scrape the name of the people in the case and Representation
+        # Regular expression pattern to extract Name, Party Type, and Representation
+        pattern = r"\s{4}(.+?)\s+(Plaintiff|Defendant)(?:\s+(.+?\(Attorney\)))?"
+        table = driver.find_element(By.ID, "paneArea.form663")
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        
+        # Find all matches
+        matches = re.findall(pattern, rows[0].text)
+
+        # Convert matches into a structured DataFrame
+        case_df = pd.DataFrame(matches, columns=["Name", "PartyType", "Representation"])
+
+        # Create a merge variable to append new information 
+        casenumber = case_info_df['CaseNumber'][0]
+        case_df['CaseNumber'] = casenumber
+        case_df['CaseNumber'].fillna(case_info_df['CaseNumber'][0], inplace=True)
+
+        # Merge data
+        combined_data = case_df.merge(case_info_df, how="left")
+        driver.back()
+
+        # 4. Get new list of elements, this time subtract first x amount of objects
+        links = driver.find_elements(By.XPATH, "//*[contains(@href, '?q=node/391/')]")
+        time.sleep(5)
+        links = links[(i+1):]
+
+        # 5. continue loop. 
+        
+        cases = pd.concat([cases, combined_data], ignore_index=True)
+    return cases
+
+def go_to_next_page(driver):
+    try:
+        arrow_table = driver.find_element(By.CLASS_NAME, "glyphicon-triangle-right")
+        arrow_table.click()
+        time.sleep(3)
+    except Exception as e:
+        print("No More Pages", e)
+        return False
+    return True
+
+def convert_date_range(start_date, end_date):
+    # Get the date range 
+    date_range = pd.date_range(start=start_date, end=end_date)
+
+    # Take the range and convert to a list
+    date_range = date_range.astype(str).to_list()
+
+    # Convert the format of the dates to fit the website
+    formatted_dates = [datetime.strptime(date, "%Y-%m-%d").strftime("%m%d%Y") for date in date_range]
+
+    return formatted_dates 
+    
---
+++ scraper-python.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from case_scrape import case_split, case_scrape, go_to_next_page, convert_date_range
+
+import re
+import time
+import pandas as pd
+from datetime import datetime
+from bs4 import BeautifulSoup
+
+'''
+PURPOSE OF THIS SCRAPER:
+This is designed to go into the ventura county court database to collect court data. 
+Saves the data to a data frame for analysis. 
+'''
+
+def scrape():
+    # Enter date range here
+    start_date = '06/01/2024'
+    end_date = '12/31/2024'
+
+    # convert date range
+    date_range = convert_date_range(start_date=start_date, end_date=end_date)
+
+
+    # Create final dataframe to store all the data. 
+    cases = pd.DataFrame(columns=['Name', 'PartyType', 'Representation', "CaseNumber", 'CaseName', 'CaseType', 'DateFiled', 'AdditionalInfo'])
+    password = open("data/password.txt", "r")
+    driver = webdriver.Chrome()
+    # Log into website
+    driver.get("https://ventura.ecourt.com/public-portal/?q=node/390")
+    username_input = driver.find_element(By.ID, "edit-name")
+    password_input = driver.find_element(By.ID, "edit-pass")
+
+    username_input.send_keys("OhOmah")
+    password_input.send_keys(password)
+
+    
+
+    # Loop through a list of dates 
+    # Query date
+    for date in date_range: 
+        # Wait until date box pops up
+        revealed = driver.find_element(By.ID, "21966")
+
+        wait = WebDriverWait(driver, timeout=10)
+        wait.until(lambda d: revealed.is_displayed())
+        revealed.clear()
+        revealed.send_keys(date)
+        submit = driver.find_element(By.ID, 'edit-submit')
+        submit.click()
+
+        # Grab the data needed 
+        while True:
+            links = driver.find_elements(By.XPATH, "//*[contains(@href, '?q=node/391/')]") 
+            # TODO: update the saving of the dateframe to account for days now. 
+            cases = case_scrape(driver,cases,links)
+
+            if not go_to_next_page(driver):
+                break
+        time.sleep(5)
+
+    # Create a loop to get a count of all clickable links in a webpage: 
+    # 1. Get the original list of elements, get the count of each element.
+    
+
+    # 2. Then loop on the length of the list
+    
+
+    
+    # if arrow is found redo loop
+    cases.to_csv('data/data.csv', index=False)
+
+if __name__ == '__main__':
+    scrape()
+
